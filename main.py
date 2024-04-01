74e40da40e936abc8a22af33ac7ea5c76a7951a9
ahernandezrod05 <ahr24011995@gmail.com>
2024-04-01 19:06:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import yt_dlp
 import os
 
-#Descargador de Audio de Youtube, sirve para Playlist y para Videos sueltos.
 def download_youtube_audio(url, output_path='./downloads'):
     # Crea el directorio si no existe
     os.makedirs(output_path, exist_ok=True)
 
     #Configuración de opciones de la libreria yt_ldp, es necesario instalar FFMPEG también.
+    #Options for the yt-dlp library, its also needed to install FFMPEG
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',  # Change to preferred audio format if needed
+            'preferredcodec': 'mp3',  # Aquí se puede cambiar el formato de audio si hace falta |Change to preferred audio format if needed
             'preferredquality': '192',
         }],
     }
     try:
         #Descarga el audio de la URL.
+        #Downloads audio URL
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
     except Exception as e:
